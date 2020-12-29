fd1e8a96328053f83df09f76f7d05d06b743d3d5
MoeGrid <1065380934@qq.com>
2020-12-29 17:40:37 +0800

    first commit

---
+++ ble.py
@@ -0,0 +1,99 @@
+import bluetooth
+import struct
+from micropython import const
+
+_ADV_TYPE_FLAGS = const(0x01)
+_ADV_TYPE_NAME = const(0x09)
+_ADV_TYPE_APPEARANCE = const(0x19)
+
+_ADV_APPEARANCE_GENERIC_COMPUTER = const(128)
+
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
+
+_UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+_UART_RX = (
+    bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
+    bluetooth.FLAG_WRITE,
+)
+_UART_SERVICE = (
+    _UART_UUID,
+    (_UART_RX, ),
+)
+
+
+class BleUtil:
+    def __init__(self, name="demo", rxbuf=64):
+        self._name = name
+
+        self._ble = bluetooth.BLE()
+        self._ble.active(True)
+        self._ble.irq(self._irq)
+        ((self._rx_handle, ), ) = self._ble.gatts_register_services((_UART_SERVICE, ))
+        self._ble.gatts_set_buffer(self._rx_handle, rxbuf, True)
+        self._connections = set()
+        self._rx_buffer = bytearray()
+        self._handler = None
+        self._payload = self._advertising_payload()
+        self._advertise()
+
+    def irq(self, handler):
+        self._handler = handler
+
+    def any(self):
+        return len(self._rx_buffer)
+
+    def read(self, sz=None):
+        if not sz:
+            sz = len(self._rx_buffer)
+        result = self._rx_buffer[0:sz]
+        self._rx_buffer = self._rx_buffer[sz:]
+        return result
+
+    def close(self):
+        for conn_handle in self._connections:
+            self._ble.gap_disconnect(conn_handle)
+        self._connections.clear()
+
+    def _irq(self, event, data):
+        if event == _IRQ_CENTRAL_CONNECT:
+            conn_handle, _, _ = data
+            self._connections.add(conn_handle)
+        elif event == _IRQ_CENTRAL_DISCONNECT:
+            conn_handle, _, _ = data
+            if conn_handle in self._connections:
+                self._connections.remove(conn_handle)
+            # 再次开始广播以允许新连接
+            self._advertise()
+        elif event == _IRQ_GATTS_WRITE:
+            conn_handle, value_handle = data
+            if conn_handle in self._connections and value_handle == self._rx_handle:
+                self._rx_buffer += self._ble.gatts_read(self._rx_handle)
+                if self._handler:
+                    self._handler()
+
+    def _advertising_payload(self):
+        payload = bytearray()
+
+        def _append(adv_type, value):
+            nonlocal payload
+            payload += struct.pack("BB", len(value) + 1, adv_type) + value
+
+        _append(_ADV_TYPE_FLAGS, struct.pack("B", 0x02 + 0x04))
+        _append(_ADV_TYPE_NAME, self._name)
+        _append(_ADV_TYPE_APPEARANCE, struct.pack("<h", _ADV_APPEARANCE_GENERIC_COMPUTER))
+        return payload
+
+    def _advertise(self, interval_us=500000):
+        self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
+
+if __name__ == "__main__":
+    ble = BleUtil()
+
+    def on_data():
+        print("rx: ", ble.read())  # .decode().strip()
+
+    ble.irq(handler=on_data)
+    ble.close()
