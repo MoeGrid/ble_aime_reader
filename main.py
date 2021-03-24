cd7da5bde34ee920700e08d23faa8f32a47fc501
MoeGrid <1065380934@qq.com>
2021-03-24 15:24:08 +0800

    增加投币功能

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import sys
+import time
 import machine
 from reader import AimeReader
 from ble import BleUtil
-from machine import UART, Timer
+from machine import UART, Timer, Pin
 
 ble = BleUtil(name='aime-reader')
 reader = AimeReader(2, 38400)
+p15 = Pin(15, Pin.OUT)
+
+def insert_coins(num):
+    print('Insert coins.')
+    for i in range(num):
+        p15.on()
+        time.sleep_ms(150)
+        p15.off()
+        time.sleep_ms(150)
 
 
 def handle_ble():
     data = ble.read()
-    if len(data) == 15:
-        p = data[0]
-        if p not in (0, 1):
+    if not data or data[0] not in (0xA1, 0xA2):
+        return
+    print(data)
+    # swipe
+    if data[0] == 0xA1:
+        p = data[1]
+        card = data[2:12]
+        if p not in (0, 1) or len(card) != 10:
             return
-        reader.swipe(p, data[5:15])
+        reader.swipe(p, card)
+    # coin
+    if data[0] == 0xA2:
+        num = data[1]
+        if num < 1 or num > 20:
+            return
+        insert_coins(num)
 
 
 if __name__ == "__main__":
     try:
         print('=== Aime Reader ===')
-        # 初始化BLE
+        # init ble
         ble.irq(handler=handle_ble)
-        # 循环处理封包
+        # wait package
         reader.wait()
     except KeyboardInterrupt:
         print('=== Exit ===')
